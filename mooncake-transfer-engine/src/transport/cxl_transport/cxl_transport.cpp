2324c4e29a65fb430377ce279004dcbdf1854d15
lianyihong <lianyihong@ieisystem.com>
2025-08-27 10:50:41 +0000

    [Transfer Engine] Fix cxl dsa for data copy

--- mooncake-transfer-engine/src/transport/cxl_transport/cxl_transport.cpp
+++ mooncake-transfer-engine/src/transport/cxl_transport/cxl_transport.cpp
@@ -32,7 +32,10 @@
 #include <fcntl.h>    // For O_RDWR, O_CREAT, etc.
 #include <unistd.h>   // For open(), close(), read(), write()
 #include <sys/mman.h> // For mmap, munmap
-// #include <dml/dml.hpp>
+
+#ifdef USE_CXL_DSA
+#include <dml/dml.hpp>
+#endif
 
 #ifdef USE_CXL_CUDA
 #include <cuda_runtime.h>
@@ -108,7 +111,8 @@ size_t CxlTransport::cxlGetDeviceSize() {
     }
     return 0;
 }
-/*
+
+#ifdef USE_CXL_DSA
 int CxlTransport::execute_copy_crc(void *dest, void *src, size_t size) {
     auto crc_seed = std::uint32_t(0u);
     char* c_src = static_cast<char*>(src);
@@ -117,23 +121,29 @@ int CxlTransport::execute_copy_crc(void *dest, void *src, size_t size) {
         return -2;
     }
     // Run operation
-    auto result = dml::execute<dml::hardware>(dml::copy_crc, dml::make_view(c_src, size),
-                    dml::make_view(c_dst, size), crc_seed);
-
-    // Check result
-    if (result.status == dml::status_code::ok) {
-        //LOG(INFO) << "DSA copy successed. from " << src << " to " << dest << " len:" << size;
-        return 0;
-    }
-    else {
-        LOG(ERROR) << "DSA copy failed：" << static_cast<int>(result.status) << std::endl;
-        //LOG(INFO) << "DSA copy failed. from " << src << " to " << dest << " len:" << size;
-        return -1;
-    }
-
-    return 0;
+    auto handler = dml::submit<dml::software>(dml::copy_crc,
+                                                dml::make_view(c_src, size),
+                                                dml::make_view(c_dst, size),
+                                                crc_seed);
+    // Wait for the result
+    int i = 0;
+    do {
+        auto result = handler.get();
+        if (result.status == dml::status_code::ok)
+        {
+            //LOG(ERROR) << "DSA copy ok:"<< ++i << std::endl;
+            return 0;
+        } else if (result.status == dml::status_code::partial_completion) {
+            LOG(ERROR) << "DSA copy partial_completion:"<< ++i << std::endl;
+            continue;
+        } else {
+            LOG(ERROR) << "DSA copy failed：" << static_cast<int>(result.status) << std::endl;
+            return -1;
+        }
+    } while(true);
+    return -1;
 }
-*/
+#endif
 
 int CxlTransport::cxlMemcpy(void *dest, void *src, size_t size) {
     // Input validation
@@ -168,20 +178,19 @@ int CxlTransport::cxlMemcpy(void *dest, void *src, size_t size) {
         LOG(INFO) << "CxlTransport::cxlMemcpy, you are using CXL_CUDA, but not using VRAM.";
     }
 #endif
-    // Perform the memory copy
-    // if (size < 32768)
-    //     std::memcpy(dest, src, size);
-    // else
-    //     execute_copy_crc(dest, src, size);
-    
-    std::memcpy(dest, src, size);
 
+    
+#ifdef USE_CXL_DSA
+    execute_copy_crc(dest, src, size);
+#else
+    // Perform the memory copy
+    std::memcpy(dest, src, size);
     // Memory barriers and cache operations
     if (isAddressInCxlRange(dest) || isAddressInCxlRange(src)) {
         // Ensure memory ordering for CXL operations
         __sync_synchronize();
     }
-
+#endif
     return 0; // success
 }
 
@@ -220,6 +229,48 @@ bool CxlTransport::isAddressInCxlRange(void *addr) {
     return (ptr >= base && ptr < end);
 }
 
+#define ALIGNMENT (2 * 1024 * 1024) // devdax 2MB alignment
+void *do_mmap_by_anonymous_fixed(size_t obj_size, int dax_fd, size_t num_dimm, size_t len_dimm) {
+    void *addr_raw;
+    size_t i,j;
+    unsigned long offset, len;
+
+    /* check alignment ??? */
+
+    addr_raw = mmap(NULL, obj_size + ALIGNMENT, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    if (addr_raw == MAP_FAILED) {
+        printf("Mmap anonymous addr failed.");
+        return NULL;
+    }
+    //memset(addr_raw, 0, obj_size + ALIGNMENT);
+
+    uintptr_t addr_int = (uintptr_t) addr_raw;
+    uintptr_t aligned_addr_int = (addr_int + ALIGNMENT - 1) & ~(ALIGNMENT - 1);
+    void * aligned_begin = (void *) aligned_addr_int;
+
+    void *cur_p = aligned_begin;
+
+    printf("Anonymous mmap addr: %p\n", aligned_begin);
+
+    size_t ext_num = (obj_size + ALIGNMENT)/(num_dimm*ALIGNMENT);
+    for (i = 0; i < ext_num; i++) {
+        offset = ALIGNMENT*i;
+        len = ALIGNMENT;
+
+        for(j = 0; j < num_dimm; j++) {
+            void *addr_tmp = mmap(cur_p, len, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, dax_fd, len_dimm*j + offset);
+            if (addr_tmp == MAP_FAILED) {
+                printf("Mmap1 extents[%ld] { offset(%lu), len(%lu) } failed.%s\n", i, offset, len, strerror(errno));
+                return NULL;
+            }
+            cur_p = (void*)((char*)cur_p + len);
+        }
+    }
+
+    printf("Anonymous mmap addr: %p\n", aligned_begin);
+    return aligned_begin;
+}
+
 int CxlTransport::cxlDevInit()
 {
     int fd = open(cxl_dev_path, O_RDWR);
@@ -228,7 +279,10 @@ int CxlTransport::cxlDevInit()
         return -1;
     }
 
-    void* ptr = mmap(NULL, cxl_dev_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    // void* ptr = mmap(NULL, cxl_dev_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    // LOG(INFO) << "CxlTransport: use normal mmap.";
+    void* ptr = do_mmap_by_anonymous_fixed(cxl_dev_size, fd, 2, 137438953472);
+    LOG(INFO) << "CxlTransport: use fixed mmap.";
     if (ptr == MAP_FAILED) {
         close(fd);
         return ERR_MEMORY;
